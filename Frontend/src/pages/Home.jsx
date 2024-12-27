46965ce5a8daa4dca7ff30ccc92ae821900cc4dc
chopricaa <124493184+chopricaa@users.noreply.github.com>
2024-12-27 13:12:28 +0530

    Updated Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 import React, { useRef, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
